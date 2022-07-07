bd54ec3deca00b2f3b4b4ec7b2a3c35274105eb3
Kitae Kim <kitaekim27@gmail.com>
2022-07-08 01:20:00 +0900

    Install executables into the initramfs verbosely

--- install-gentoo.sh
+++ install-gentoo.sh
@@ -247,7 +247,7 @@ EOF
 
     info "set the initramfs directory layout"
     mkdir -p /usr/src/initramfs/{bin,dev,mnt/root,etc,lib,lib64,proc,root,sbin,sys}
-    install init /usr/src/initramfs/
+    install --verbose init /usr/src/initramfs/
 
     info "install dependencies for building an initramfs"
     emerge --ask --tree --verbose sys-apps/busybox
