05a2d658f605f60688e3e2944045ef562afb5b36
Kitae Kim <kitaekim27@gmail.com>
2022-07-10 00:35:46 +0900

    Install system management tools (gentoolkit)

--- install-gentoo.sh
+++ install-gentoo.sh
@@ -15,8 +15,13 @@
 
 set -o errexit -o nounset -o noglob -o pipefail
 
-print() { echo "$(basename "$0"):" "$@" ; }
-error() { print "$@" >&2 ; }
+info() {
+    echo "$(basename "$0"):" "$@"
+}
+
+error() {
+    info "$@" >&2
+}
 
 readpass() {
     local checkvar=
@@ -33,7 +38,7 @@ readpass() {
         then
             break
         else
-            print "You've entered different passphrases! Try again."
+            info "You've entered different passphrases! Try again."
         fi
     done
 }
@@ -44,20 +49,20 @@ then
     exit 1
 fi
 
-print "Sync the system time with ntp.org."
+info "Sync the system time with ntp.org."
 ntpd -q -g
 
-print "Configure Portage mirrors."
+info "Configure Portage mirrors."
 mirrorselect --servers="5"
 
-print "Install my scripts into the system."
+info "Install my scripts into the system."
 find tools -mindepth 1 -maxdepth 1 \
     -exec cp --verbose --recursive {} /usr/local/bin/ \;
 
 # Note that currently tpm2-tools package is in testing branch. that means, you
 # need to unmask the package to install. Remove --autounmask flag when
 # tpm2-tools package is in stable branch.
-print "Install package tpm2-tools."
+info "Install package tpm2-tools."
 # FIXME: For some reason, emerge with --autounmask returns 1 when it succeeds.
 emerge --ask --tree --verbose --autounmask app-crypt/tpm2-tools || [ "$?" = 1 ]
 dispatch-conf
@@ -87,29 +92,29 @@ read -rp "Select a root partition: " partition_root
 read -rp "Select an efi partition: " partition_efi
 read -rp "Select a swap partition: " partition_swap
 
-print "Format an efi partition in vfat."
+info "Format an efi partition in vfat."
 mkfs.vfat "/dev/$partition_efi"
 
-print "Set swap on $partition_swap."
+info "Set swap on $partition_swap."
 mkswap "/dev/$partition_swap"
 swapon "/dev/$partition_swap"
 
 tmpdir=$(mktemp --directory)
 
-print "Create a primary object in the endorsement hierarchy."
+info "Create a primary object in the endorsement hierarchy."
 tpm2 createprimary --quiet --hierarchy="e" --key-context="$tmpdir/primary.ctx"
 
-print "Start a TPM HMAC trial session for building a policy."
+info "Start a TPM HMAC trial session for building a policy."
 tpm2 startauthsession --session="$tmpdir/session.bin"
 
-print "Generate a TPM policy with sha256 bank of PCR 0,2,4,8,9."
+info "Generate a TPM policy with sha256 bank of PCR 0,2,4,8,9."
 tpm2 policypcr \
     --quiet \
     --session="$tmpdir/session.bin" \
     --pcr-list="sha256:0,2,4,8,9" \
     --policy="$tmpdir/policy.bin"
 
-print "Seal a random key into a TPM object with the policy."
+info "Seal a random key into a TPM object with the policy."
 dd if=/dev/urandom bs=128 count=1 status=none | tpm2 create \
     --quiet \
     --parent-context="$tmpdir/primary.ctx" \
@@ -120,17 +125,17 @@ dd if=/dev/urandom bs=128 count=1 status=none | tpm2 create \
 # Note that 0x81018000 is the first non-reserved persistent object handle in the
 # endorsement hierarchy.
 # See: TCG, "Registry of Reserved TPM 2.0 Handles and Localities"
-print "Make the generated key object persistent in TPM."
+info "Make the generated key object persistent in TPM."
 # Try to evict existing object at 0x81018000 first.
 # XXX: Make sure that there are no important objects at 0x81018000.
 tpm2 evictcontrol --object-context="0x81018000" 2>/dev/null || :
 tpm2 evictcontrol --object-context="$tmpdir/key.ctx" 0x81018000
 
-print "Create a LUKS2 passphrase for the root partition."
+info "Create a LUKS2 passphrase for the root partition."
 readpass "Enter a passphrase for the root partition: " passphrase
 luks_passphrase=$(mkpassphrase "${passphrase?}" | sed -n "s/result = \(.*\)/\1/p")
 
-print "Encrypt the root partition."
+info "Encrypt the root partition."
 echo "$luks_passphrase" \
     | xxd -revert -plain \
     | cryptsetup luksFormat \
@@ -138,7 +143,7 @@ echo "$luks_passphrase" \
         --key-file="-" \
         "/dev/$partition_root"
 
-print "Add a recovery passphrase for the root partition."
+info "Add a recovery passphrase for the root partition."
 readpass "Enter a recovery passphrase for the root partition: " recovery_passphrase
 echo "$luks_passphrase" \
     | xxd -revert -plain \
@@ -147,7 +152,7 @@ echo "$luks_passphrase" \
         "/dev/$partition_root" \
         <(echo "${recovery_passphrase?}")
 
-print "Decrypt the root partition."
+info "Decrypt the root partition."
 echo "$luks_passphrase" \
     | xxd -revert -plain \
     | cryptsetup luksOpen \
@@ -155,20 +160,20 @@ echo "$luks_passphrase" \
         "/dev/$partition_root" \
         root
 
-print "Format the root partition in btrfs."
+info "Format the root partition in btrfs."
 mkfs.btrfs /dev/mapper/root
 
-print "Mount the root partition."
+info "Mount the root partition."
 mkdir --parents /mnt/root
 mount /dev/mapper/root /mnt/root
 
-print "Set the filesystem hierarchy layout in the root partition."
+info "Set the filesystem hierarchy layout in the root partition."
 mkdir --parents /mnt/root/boot
 mkdir --parents /mnt/root/deploy/{image-a,image-b}
 mkdir --parents /mnt/root/snapshots
 
 # TODO: Verify the downloaded files.
-print "Download a stage 3 tarball."
+info "Download a stage 3 tarball."
 readonly ARCH="amd64"
 readonly MIRROR="http://ftp.kaist.ac.kr/gentoo/releases"
 wget --recursive --no-parent --no-directories \
@@ -178,91 +183,94 @@ wget --recursive --no-parent --no-directories \
 
 readonly install_root="/mnt/root/deploy/image-a"
 
-print "Extract the stage 3 tarball into $install_root."
+info "Extract the stage 3 tarball into $install_root."
 find download -iname "stage3-*.tar.xz" -exec tar \
     --directory="$install_root" \
     --extract --preserve-permissions --file={} \
     --xattrs-include='*.*' --numeric-owner \;
 
-print "Configure the DNS of the installation."
+info "Configure the DNS of the installation."
 cp --dereference /etc/resolv.conf "$install_root/etc/resolv.conf"
 
-print "Install config files into the installation."
+info "Install config files into the installation."
 find config -mindepth 1 -maxdepth 1 \
     -exec cp --recursive --preserve {} "$install_root" \;
 
-print "Install my scripts into the installation."
+info "Install my scripts into the installation."
 find tools -mindepth 1 -maxdepth 1 \
     -exec cp --recursive {} "$install_root/usr/local/bin/" \;
 
-print "Set the initramfs source directory in the installation."
+info "Set the initramfs source directory in the installation."
 mkdir --parents $install_root/usr/src/initramfs/{mnt/root,usr/bin,\
                                             usr/local/bin,bin,sbin,dev,proc,sys}
 find initramfs -mindepth 1 -maxdepth 1 \
     -exec cp --recursive {} "$install_root/usr/src/initramfs" \;
 
-print "Mount the proc filesystem in the installation."
+info "Mount the proc filesystem in the installation."
 mount --types proc /proc "$install_root/proc"
 
-print "Mount the sys fileseystem in the installation."
+info "Mount the sys fileseystem in the installation."
 mount --rbind /sys "$install_root/sys"
 mount --make-rslave "$install_root/sys"
 
-print "Mount the dev filesystem in the installation."
+info "Mount the dev filesystem in the installation."
 mount --rbind /dev "$install_root/dev"
 mount --make-rslave "$install_root/dev"
 
-print "Mount the run filesystem in the installation."
+info "Mount the run filesystem in the installation."
 mount --bind /run "$install_root/run"
 mount --make-rslave "$install_root/run"
 
 chroot_main() {
-    print "Mount an efi partition."
+    info "Mount an efi partition."
     mount "/dev/$partition_efi" /boot
 
-    print "Configure Portage ebuild repositories."
+    info "Configure Portage ebuild repositories."
     mkdir --parents /etc/portage/repos.conf
     cp /usr/share/portage/config/repos.conf \
         /etc/portage/repos.conf/gentoo.conf
 
-    print "Install a snapshot of the Gentoo ebuild repository."
+    info "Install a snapshot of the Gentoo ebuild repository."
     emerge-webrsync
 
-    print "Choose a Portage profile."
+    info "Choose a Portage profile."
     eselect profile list
     read -rp "Select a Portage profile: " profile
     eselect profile set "$profile"
 
     if [ -d /etc/portage/package.use ]
     then
-        print "Make the Portage package.use a single file"
+        info "Make the Portage package.use a single file"
         rm --recursive /etc/portage/package.use
         touch /etc/portage/package.use
     fi
 
-    print "Update @world Portage set."
+    info "Update @world Portage set."
     emerge --verbose --update --deep --newuse @world
 
-    print "Set the timezone to Asia/Seoul."
+    info "Install system management tools."
+    emerge --verbose --tree app-portage/gentoolkit
+
+    info "Set the timezone to Asia/Seoul."
     echo "Asia/Seoul" > /etc/timezone
     emerge --config sys-libs/timezone-data
 
-    print "Generate the locales."
+    info "Generate the locales."
     nano -w /etc/locale.gen
     locale-gen
 
-    print "Configure the locales."
+    info "Configure the locales."
     eselect locale list
     read -rp "Select a locale: " locale
     eselect locale set "$locale"
 
-    print "Reload the environment."
+    info "Reload the environment."
     env-update
 
-    # This process can be automated using something like cpuid or /proc/cpuprint.
-    print "Install microcodes for your CPU."
-    print " 1. Intel"
-    print " 2. AMD"
+    # This process can be automated using something like cpuid or /proc/cpuinfo.
+    info "Install microcodes for your CPU."
+    info " 1. Intel"
+    info " 2. AMD"
     while true
     do
         read -rp "Select your CPU manufacturer: " cpu
@@ -270,17 +278,17 @@ chroot_main() {
             1 | intel | Intel | INTEL)
                 # This USE flag generates microcode cpio at /boot so that GRUB automatically
                 # detect and generate config with it.
-                print "Enable USE flag \"initramfs\" of sys-firmware/intel-microcode"
+                info "Enable USE flag \"initramfs\" of sys-firmware/intel-microcode"
                 echo "sys-firmware/intel-microcode initramfs" >> /etc/portage/package.use
 
-                print "Install the intel microcode package."
+                info "Install the intel microcode package."
                 emerge --tree --verbose sys-firmware/intel-microcode
 
                 break
                 ;;
             2 | amd | Amd | AMD)
                 # AMD microcodes are shipped in the sys-kernel/linux-firmware package.
-                print "Enable USE flag \"initramfs\" of sys-kernel/linux-firmware"
+                info "Enable USE flag \"initramfs\" of sys-kernel/linux-firmware"
                 echo "sys-kernel/linux-firmware initramfs" >> /etc/portage/package.use
 
                 break
@@ -291,49 +299,49 @@ chroot_main() {
         esac
     done
 
-    print "Install the linux firmwares."
+    info "Install the linux firmwares."
     echo "sys-kernel/linux-firmware linux-fw-redistributable no-source-code" \
         >> /etc/portage/package.license
     emerge --tree --verbose sys-kernel/linux-firmware
 
-    print "Install the linux kernel sources."
+    info "Install the linux kernel sources."
     emerge --tree --verbose sys-kernel/gentoo-sources
 
     # /usr/src/linux symlink refers to the source tree corresponding with the
     # currently running kernel.
-    print "Create a symlink /usr/src/linux."
+    info "Create a symlink /usr/src/linux."
     eselect kernel list
     read -rp "Select a kernel: " kernel
     eselect kernel set "$kernel"
 
     make="make --directory=/usr/src/linux --jobs=$(nproc)"
 
-    print "Configure the linux kernel."
+    info "Configure the linux kernel."
     $make menuconfig
 
-    print "Compile and install the linux kernel."
+    info "Compile and install the linux kernel."
     $make
     $make modules_install
     # This will copy the kernel image into /boot together with the System.map
     # file and the kernel configuration file.
     $make install
 
-    print "Install packages for building an initramfs."
+    info "Install packages for building an initramfs."
     emerge --tree --verbose sys-apps/busybox sys-fs/cryptsetup app-arch/lz4
 
     # Note that currently tpm2-tools package is in testing branch. that means, you
     # need to unmask the package to install. Remove --autounmask flag when
     # tpm2-tools package is in stable branch.
-    print "Install tpm2-tools to build an initramfs."
+    info "Install tpm2-tools to build an initramfs."
     # FIXME: For some reason, emerge with --autounmask returns 1 when it succeeds.
     emerge --ask --tree --verbose --autounmask app-crypt/tpm2-tools || [ "$?" = 1 ]
     dispatch-conf
     emerge --tree --verbose app-crypt/tpm2-tools
 
-    print "Build and install an initramfs."
+    info "Build and install an initramfs."
     mkinitramfs
 
-    print "Generate a fstab."
+    info "Generate a fstab."
     cat << EOF > /etc/fstab
 PARTUUID=$(blkid -o value -s PARTUUID "/dev/$partition_efi")     /boot    vfat     noauto,noatime                                               0 0
 PARTUUID=$(blkid -o value -s PARTUUID "/dev/$partition_swap")    none     swap     sw                                                           0 0
@@ -343,24 +351,24 @@ PARTUUID=$(blkid -o value -s PARTUUID "/dev/$partition_swap")    none     swap  
 PARTUUID=$(blkid -o value -s PARTUUID "/dev/$partition_root")    /        btrfs    noatime,rw,space_cache=v2,subvloid=5,subvol=/deploy/taget    0 1
 EOF
 
-    print "Set the hostname."
+    info "Set the hostname."
     nano /etc/conf.d/hostname
 
-    print "Set the hosts information."
+    info "Set the hosts information."
     nano /etc/hosts
 
-    print "Install NetworkManager."
+    info "Install NetworkManager."
     emerge --tree --verbose net-misc/networkmanager
 }
 
-print "chroot into $install_root and execute chroot_main()."
+info "chroot into $install_root and execute chroot_main()."
 chroot "$install_root" /bin/bash -c "
     set -o errexit -o nounset -o noglob -o pipefail
     root_storage=$root_storage
     partition_efi=$partition_efi
     partition_swap=$partition_swap
     partition_root=$partition_root
-    $(declare -f print)
+    $(declare -f info)
     $(declare -f error)
     $(declare -f chroot_main)
     chroot_main
