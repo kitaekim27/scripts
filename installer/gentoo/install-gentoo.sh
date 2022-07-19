a7919c32e9fb6189de2be28229b748b6c03d5395
Kitae Kim <kitaekim27@gmail.com>
2022-07-20 06:52:16 +0900

    Cleanup

--- installer/gentoo/install-gentoo.sh
+++ installer/gentoo/install-gentoo.sh
@@ -18,34 +18,34 @@ set -o errexit -o nounset -o noglob -o pipefail
 this_script=$(basename "$0")
 
 info() {
-    echo "${this_script}:" "$@"
+	echo "${this_script}:" "$@"
 }
 
 error() {
-    info "$@" >&2
+	info "$@" >&2
 }
 
 get_passphrase() {
-    while true
-    do
-        read -srp "${1?}" "${2?}"
-        echo
-        read -srp "Enter a passphrase again: " checkphrase
-        echo
+	while true
+	do
+		read -srp "${1?}" "${2?}"
+		echo
+		read -srp "Enter a passphrase again: " checkphrase
+		echo
 
-        if [ "$(eval echo "\$${2?}")" = "${checkphrase?}" ]
-        then
-            break
-        else
-            info "You've entered different passphrases! Try again."
-        fi
-    done
+		if [ "$(eval echo "\$${2?}")" = "${checkphrase?}" ]
+		then
+			break
+		else
+			info "You've entered different passphrases! Try again."
+		fi
+	done
 }
 
 if [ "$EUID" != 0 ]
 then
-    error "This script requires root privilege!"
-    exit 1
+	error "This script requires root privilege!"
+	exit 1
 fi
 
 info "Sync the system time with ntp.org."
@@ -56,15 +56,15 @@ mirrorselect --interactive
 
 info "Install my scripts into the system."
 find -L tools -mindepth 1 -maxdepth 1 \
-    -exec cp --verbose --recursive {} /usr/local/bin/ \;
+	-exec cp --verbose --recursive {} /usr/local/bin/ \;
 
 info "Install package tpm2-tools."
 emerge --autounmask --autounmask-continue app-crypt/tpm2-tools
 
 if ! nc -zw1 google.com 443
 then
-    error "Can not connect to the internet!"
-    exit 1
+	error "Can not connect to the internet!"
+	exit 1
 fi
 
 lsblk
@@ -93,22 +93,22 @@ luks_passphrase=$(mkpassphrase "${passphrase?}" | sed -n "s/result = \(.*\)/\1/p
 
 info "Encrypt the root partition."
 echo "${luks_passphrase}" | xxd -revert -plain | cryptsetup luksFormat \
-    --type="luks2" \
-    --key-file="-" \
-    "/dev/${partition_root}"
+	--type="luks2" \
+	--key-file="-" \
+	"/dev/${partition_root}"
 
 info "Add a secondary passphrase for the root partition."
 get_passphrase "Enter a secondary phrase for the root partition: " secondary_phrase
 echo "${luks_passphrase}" | xxd -revert -plain | cryptsetup luksAddKey \
-    --key-file="-" \
-    "/dev/${partition_root}" \
-    <(printf "%s" "${recovery_phrase?}")
+	--key-file="-" \
+	"/dev/${partition_root}" \
+	<(printf "%s" "${recovery_phrase?}")
 
 info "Decrypt the root partition."
 echo "${luks_passphrase}" | xxd -revert -plain | cryptsetup luksOpen \
-    --key-file="-" \
-    "/dev/${partition_root}" \
-    root
+	--key-file="-" \
+	"/dev/${partition_root}" \
+	root
 
 info "Format the root partition in btrfs."
 mkfs.btrfs /dev/mapper/root
@@ -129,24 +129,24 @@ ln --symbolic /deploy/image-a /mnt/root/deploy/current
 ln --symbolic /deploy/image-a /mnt/root/deploy/target
 # This makes mounttab valid so that we can see / mount entry inside the chroot.
 mount --types="btrfs" --options="noatime,subvol=/deploy/target" \
-    /dev/mapper/root /mnt/root/deploy/image-a
+	/dev/mapper/root /mnt/root/deploy/image-a
 
 # TODO: Verify the downloaded files.
 info "Download a stage 3 tarball."
 readonly ARCH="amd64"
 readonly MIRROR="http://ftp.kaist.ac.kr/gentoo/releases"
 wget --recursive --no-parent --no-directories \
-    --directory-prefix="download" \
-    --accept="stage3-$ARCH-hardened-openrc-*" \
-    "$MIRROR/$ARCH/autobuilds/current-stage3-$ARCH-openrc/"
+	--directory-prefix="download" \
+	--accept="stage3-$ARCH-hardened-openrc-*" \
+	"$MIRROR/$ARCH/autobuilds/current-stage3-$ARCH-openrc/"
 
 readonly INSTALL_ROOT="/mnt/root/deploy/image-a"
 
 info "Extract the stage 3 tarball into $INSTALL_ROOT."
 find download -iname "stage3-*.tar.xz" -exec tar \
-    --directory="$INSTALL_ROOT" \
-    --extract --preserve-permissions --file={} \
-    --xattrs-include='*.*' --numeric-owner \;
+	--directory="$INSTALL_ROOT" \
+	--extract --preserve-permissions --file={} \
+	--xattrs-include='*.*' --numeric-owner \;
 
 info "Mount the proc filesystem in the installation."
 mount --types="proc" /proc "$INSTALL_ROOT/proc"
@@ -164,166 +164,166 @@ mount --bind /run "$INSTALL_ROOT/run"
 mount --make-rslave "$INSTALL_ROOT/run"
 
 chroot_cleanup() {
-    info "Clean up installation artiparcts."
-    rm --recursive /config
+	info "Clean up installation artiparcts."
+	rm --recursive /config
 }
 
 chroot_main() {
-    info "Mount an efi partition."
-    mount "/dev/${partition_uefi}" /boot
+	info "Mount an efi partition."
+	mount "/dev/${partition_uefi}" /boot
 
-    info "Generate the fstab into the installation."
-    PARTUUID_UEFI=$(blkid -o value -s PARTUUID "/dev/${partition_uefi}") \
-    PARTUUID_SWAP=$(blkid -o value -s PARTUUID "/dev/${partition_swap}") \
-    PARTUUID_ROOT=$(blkid -o value -s PARTUUID "/dev/${partition_root}") \
-        envsubst < /config/etc/fstab.tmpl > /etc/fstab
+	info "Generate the fstab into the installation."
+	PARTUUID_UEFI=$(blkid -o value -s PARTUUID "/dev/${partition_uefi}") \
+	PARTUUID_SWAP=$(blkid -o value -s PARTUUID "/dev/${partition_swap}") \
+	PARTUUID_ROOT=$(blkid -o value -s PARTUUID "/dev/${partition_root}") \
+		envsubst < /config/etc/fstab.tmpl > /etc/fstab
 
-    if [ -d /etc/portage/package.use ]
-    then
-        info "Make the Portage package.use a single file."
-        rm --recursive /etc/portage/package.use
-        touch /etc/portage/package.use
-    fi
+	if [ -d /etc/portage/package.use ]
+	then
+		info "Make the Portage package.use a single file."
+		rm --recursive /etc/portage/package.use
+		touch /etc/portage/package.use
+	fi
 
-    info "Configure the Portage make.conf file."
-    install --mode="600" /config/etc/portage/make.conf /etc/portage/make.conf
-    echo "MAKEOPTS=\"-j$(nproc)\"" >> /etc/portage/make.conf
+	info "Configure the Portage make.conf file."
+	install --mode="600" /config/etc/portage/make.conf /etc/portage/make.conf
+	echo "MAKEOPTS=\"-j$(nproc)\"" >> /etc/portage/make.conf
 
-    info "Configure the Portage ebuild repositories."
-    mkdir --parents /etc/portage/repos.conf
-    cp /usr/share/portage/config/repos.conf /etc/portage/repos.conf/gentoo.conf
-    emerge-webrsync
+	info "Configure the Portage ebuild repositories."
+	mkdir --parents /etc/portage/repos.conf
+	cp /usr/share/portage/config/repos.conf /etc/portage/repos.conf/gentoo.conf
+	emerge-webrsync
 
-    info "Install system management tools."
-    emerge app-portage/gentoolkit app-portage/mirrorselect sys-fs/btrfs-progs
+	info "Install system management tools."
+	emerge app-portage/gentoolkit app-portage/mirrorselect sys-fs/btrfs-progs
 
-    info "Set the system-wide USE flags."
-    euse --enable elogind X bluetooth networkmanager dbus
-    euse --disable systemd
+	info "Set the system-wide USE flags."
+	euse --enable elogind X bluetooth networkmanager dbus
+	euse --disable systemd
 
-    info "Configure Portage mirrors."
-    mirrorselect --interactive
+	info "Configure Portage mirrors."
+	mirrorselect --interactive
 
-    info "Choose a Portage profile."
-    eselect profile list
-    read -rp "Select a Portage profile: " profile
-    eselect profile set "${profile}"
+	info "Choose a Portage profile."
+	eselect profile list
+	read -rp "Select a Portage profile: " profile
+	eselect profile set "${profile}"
 
-    info "Update @world Portage set."
-    emerge --update --deep --newuse @world
+	info "Update @world Portage set."
+	emerge --update --deep --newuse @world
 
-    info "Set the timezone to Asia/Seoul."
-    echo "Asia/Seoul" > /etc/timezone
-    emerge --config sys-libs/timezone-data
+	info "Set the timezone to Asia/Seoul."
+	echo "Asia/Seoul" > /etc/timezone
+	emerge --config sys-libs/timezone-data
 
-    info "Generate system locales."
-    nano -w /etc/locale.gen
-    locale-gen
+	info "Generate system locales."
+	nano -w /etc/locale.gen
+	locale-gen
 
-    info "Configure system locales."
-    eselect locale list
-    read -rp "Select a locale: " locale
-    eselect locale set "${locale}"
+	info "Configure system locales."
+	eselect locale list
+	read -rp "Select a locale: " locale
+	eselect locale set "${locale}"
 
-    info "Reload the environment."
-    env-update
-    source /etc/profile
+	info "Reload the environment."
+	env-update
+	source /etc/profile
 
-    if grep --quiet --ignore-case "AMD" /proc/cpuinfo
-    then
-        info "AMD CPU detected. Install AMD microcodes."
-        # AMD microcodes are shipped in the sys-kernel/linux-firmware package.
-        info "Enable USE flag \"initramfs\" of sys-kernel/linux-firmware"
-        echo "sys-kernel/linux-firmware initramfs" >> /etc/portage/package.use
+	if grep --quiet --ignore-case "AMD" /proc/cpuinfo
+	then
+		info "AMD CPU detected. Install AMD microcodes."
+		# AMD microcodes are shipped in the sys-kernel/linux-firmware package.
+		info "Enable USE flag \"initramfs\" of sys-kernel/linux-firmware"
+		echo "sys-kernel/linux-firmware initramfs" >> /etc/portage/package.use
 
-    elif grep --quiet --ignore-case "Intel" /proc/cpuinfo
-    then
-        info "Intel CPU detected. Install Intel microcodes."
-        info "Enable USE flag \"initramfs\" of sys-firmware/intel-microcode"
-        # This USE flag generates microcode cpio at /boot so that GRUB automatically
-        # detect and generate config with it.
-        echo "sys-firmware/intel-microcode initramfs" >> /etc/portage/package.use
+	elif grep --quiet --ignore-case "Intel" /proc/cpuinfo
+	then
+		info "Intel CPU detected. Install Intel microcodes."
+		info "Enable USE flag \"initramfs\" of sys-firmware/intel-microcode"
+		# This USE flag generates microcode cpio at /boot so that GRUB automatically
+		# detect and generate config with it.
+		echo "sys-firmware/intel-microcode initramfs" >> /etc/portage/package.use
 
-        info "Install the intel microcode package."
-        emerge sys-firmware/intel-microcode
-    else
-        info "Can not determine CPU manufacturer. Do not install microcodes."
-    fi
+		info "Install the intel microcode package."
+		emerge sys-firmware/intel-microcode
+	else
+		info "Can not determine CPU manufacturer. Do not install microcodes."
+	fi
 
-    info "Install the linux firmwares."
-    echo "sys-kernel/linux-firmware linux-fw-redistributable no-source-code" \
-        >> /etc/portage/package.license
-    emerge sys-kernel/linux-firmware
+	info "Install the linux firmwares."
+	echo "sys-kernel/linux-firmware linux-fw-redistributable no-source-code" \
+		>> /etc/portage/package.license
+	emerge sys-kernel/linux-firmware
 
-    info "Install the linux kernel sources."
-    emerge sys-kernel/gentoo-sources
+	info "Install the linux kernel sources."
+	emerge sys-kernel/gentoo-sources
 
-    # /usr/src/linux symlink refers to the source tree corresponding to the
-    # currently running kernel.
-    info "Create a symlink /usr/src/linux."
-    eselect kernel list
-    read -rp "Select a linux kernel to use: " kernel
-    eselect kernel set "${kernel}"
+	# /usr/src/linux symlink refers to the source tree corresponding to the
+	# currently running kernel.
+	info "Create a symlink /usr/src/linux."
+	eselect kernel list
+	read -rp "Select a linux kernel to use: " kernel
+	eselect kernel set "${kernel}"
 
-    make="make --directory=/usr/src/linux --jobs=$(nproc)"
+	make="make --directory=/usr/src/linux --jobs=$(nproc)"
 
-    info "Configure the linux kernel."
-    ${make} defconfig
-    ${make} menuconfig
+	info "Configure the linux kernel."
+	${make} defconfig
+	${make} menuconfig
 
-    info "Compile and install the linux kernel."
-    ${make}
-    ${make} modules_install
-    # This will copy the kernel image into /boot together with the System.map
-    # file and the kernel configuration file.
-    ${make} install
+	info "Compile and install the linux kernel."
+	${make}
+	${make} modules_install
+	# This will copy the kernel image into /boot together with the System.map
+	# file and the kernel configuration file.
+	${make} install
 
-    info "Install packages to build an initramfs."
-    emerge sys-fs/cryptsetup app-shells/fzf
-    emerge --autounmask --autounmask-continue app-crypt/tpm2-tools
+	info "Install packages to build an initramfs."
+	emerge sys-fs/cryptsetup app-shells/fzf
+	emerge --autounmask --autounmask-continue app-crypt/tpm2-tools
 
-    info "Build and install an initramfs."
-    mkinitramfs
+	info "Build and install an initramfs."
+	mkinitramfs
 
-    info "Set the hostname."
-    read -rp "Enter the hostname: " hostname
-    sed -i "s/localhost/${hostname}/g" /etc/conf.d/hostname /etc/hosts
+	info "Set the hostname."
+	read -rp "Enter the hostname: " hostname
+	sed -i "s/localhost/${hostname}/g" /etc/conf.d/hostname /etc/hosts
 
-    info "Enable the elogind service."
-    rc-update add elogind boot
+	info "Enable the elogind service."
+	rc-update add elogind boot
 
-    info "Install system daemons."
-    emerge sys-process/fcron net-misc/chrony net-misc/networkmanager app-admin/rsyslog
+	info "Install system daemons."
+	emerge sys-process/fcron net-misc/chrony net-misc/networkmanager app-admin/rsyslog
 
-    info "Register system daemons to openrc runlevels."
-    rc-update add fcron default
-    rc-update add chronyd default
-    rc-update add NetworkManager default
-    rc-update add rsyslog default
+	info "Register system daemons to openrc runlevels."
+	rc-update add fcron default
+	rc-update add chronyd default
+	rc-update add NetworkManager default
+	rc-update add rsyslog default
 
-    info "Install system packages."
-    emerge app-admin/doas \
-        && install --mode="600" config/etc/doas.conf /etc/doas.conf
-    echo "app-admin/logrotate cron" >> /etc/portage/package.use \
-        && emerge app-admin/logrotate
+	info "Install system packages."
+	emerge app-admin/doas \
+		&& install --mode="600" config/etc/doas.conf /etc/doas.conf
+	echo "app-admin/logrotate cron" >> /etc/portage/package.use \
+		&& emerge app-admin/logrotate
 
-    info "Install GRUB2 bootloader."
-    emerge sys-boot/grub
-    grub-install --target="x86_64-efi" --efi-directory="/boot" --removable
-    echo "GRUB_CMDLINE_LINUX=\"root=PARTUUID=$(blkid -o value -s PARTUUID /dev/"${partition_root}")\"" \
-        >> /etc/default/grub
-    grub-mkconfig --output="/boot/grub/grub.cfg"
+	info "Install GRUB2 bootloader."
+	emerge sys-boot/grub
+	grub-install --target="x86_64-efi" --efi-directory="/boot" --removable
+	echo "GRUB_CMDLINE_LINUX=\"root=PARTUUID=$(blkid -o value -s PARTUUID /dev/"${partition_root}")\"" \
+		>> /etc/default/grub
+	grub-mkconfig --output="/boot/grub/grub.cfg"
 
-    info "Create a default user."
-    read -rp "Enter an user name: " user
-    useradd --create-home --groups="users,wheel" --shell="/bin/bash" "${user}"
-    passwd "${user}"
-    install --mode="644" /config/home/user/dot-profile "/home/${user}/.profile"
+	info "Create a default user."
+	read -rp "Enter an user name: " user
+	useradd --create-home --groups="users,wheel" --shell="/bin/bash" "${user}"
+	passwd "${user}"
+	install --mode="644" /config/home/user/dot-profile "/home/${user}/.profile"
 
-    info "Install XOrg server."
-    emerge x11-base/xorg-server
-    env-update
-    source /etc/profile
+	info "Install XOrg server."
+	emerge x11-base/xorg-server
+	env-update
+	source /etc/profile
 }
 
 info "Configure DNS of the installation."
@@ -331,27 +331,27 @@ cp --dereference /etc/resolv.conf "$INSTALL_ROOT/etc/resolv.conf"
 
 info "Set the initramfs source directory in the installation."
 find initramfs -mindepth 1 -maxdepth 1 \
-    -exec cp --verbose --preserve --recursive {} "$INSTALL_ROOT/usr/src/initramfs" \;
+	-exec cp --verbose --preserve --recursive {} "$INSTALL_ROOT/usr/src/initramfs" \;
 
 info "Copy config files into the installation."
 cp --verbose --recursive config "$INSTALL_ROOT"
 
 info "Install my scripts into the installation."
 find -L tools -mindepth 1 -maxdepth 1 \
-    -exec cp --verbose --recursive {} "$INSTALL_ROOT/usr/local/bin/" \;
+	-exec cp --verbose --recursive {} "$INSTALL_ROOT/usr/local/bin/" \;
 
 info "chroot into $INSTALL_ROOT and execute chroot_main()."
 chroot "$INSTALL_ROOT" /bin/bash -c "
-    set -o errexit -o nounset -o noglob -o pipefail
-    this_script=${this_script}
-    root_storage=${root_storage}
-    partition_uefi=${partition_uefi}
-    partition_swap=${partition_swap}
-    partition_root=${partition_root}
-    $(declare -f info)
-    $(declare -f error)
-    $(declare -f chroot_cleanup)
-    $(declare -f chroot_main)
-    chroot_main
-    chroot_cleanup
+	set -o errexit -o nounset -o noglob -o pipefail
+	this_script=${this_script}
+	root_storage=${root_storage}
+	partition_uefi=${partition_uefi}
+	partition_swap=${partition_swap}
+	partition_root=${partition_root}
+	$(declare -f info)
+	$(declare -f error)
+	$(declare -f chroot_cleanup)
+	$(declare -f chroot_main)
+	chroot_main
+	chroot_cleanup
 "
