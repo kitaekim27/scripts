19e2b855620d01f30c8937a4f5f29a394168ad33
Kitae Kim <kitaekim27@gmail.com>
2022-07-30 04:20:58 +0900

    emacs

--- installer/gentoo/config/emacs/init.el
+++ installer/gentoo/config/emacs/init.el
@@ -2,12 +2,12 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
 (package-initialize)
+(package-refresh-contents)
 
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
-(eval-when-compile
-  (require 'use-package))
+(eval-when-compile (require 'use-package))
 
 (use-package vterm :ensure t)
 
@@ -18,4 +18,19 @@
 (use-package evil-collection :ensure t)
 (evil-collection-init)
 
-(use-package spacemacs-theme :ensure t)
+(use-package all-the-icons :ensure t)
+
+(use-package doom-themes
+  :ensure t
+  :config
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
+  (load-theme 'doom-one t)
+  (doom-themes-visual-bell-config)
+  (doom-themes-neotree-config)
+  (setq doom-themes-treemacs-theme "doom-atom")
+  (doom-themes-treemacs-config)
+  (doom-themes-org-config))
+
+(use-package doom-modeline :ensure t
+  :init (doom-modeline-mode 1))
